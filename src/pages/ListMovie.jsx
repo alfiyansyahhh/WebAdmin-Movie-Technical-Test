0d1d6b1e672bfda3063cc21465926475d42a1272
alfiyansyah <65682948+alfiyansyahhh@users.noreply.github.com>
2022-01-11 22:43:32 +0700

    update pagination

--- src/pages/ListMovie.jsx
+++ src/pages/ListMovie.jsx
@@ -1,12 +1,97 @@
 import Navbar from '../components/Navbar'
+import { useEffect, useState, useMemo } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { ACTION_GET_MOVIES } from '../redux/actions/ListMovie'
+import './css/ListMovie.css'
+import {Row, Col} from 'reactstrap';
+import Pagino from "pagino";
+import { useNavigate } from 'react-router-dom';
 
-const ListMoovie = () => {
+const ListMovie = () => {
+
+    const dispatch = useDispatch()
+    const ListMovies= useSelector((state) => state.ListMovie)
+
+
+    useEffect(() => {
+        dispatch(ACTION_GET_MOVIES(1))
+    }, [])
+
+
+    const [pages, setPages] = useState([]);
+
+    const pagino = useMemo(() => {
+        const _ = new Pagino({
+        onChange: (page, count) => setPages(_.getPages()) 
+
+        });
+
+        _.setCount(20);
+
+        return _;
+    }, []);
+
+    const hanglePaginoNavigation = (type) => {
+        if (typeof type === "string") {
+        pagino[type]?.();
+        return;
+        }
+
+        pagino.setPage(type);
+    };
+
+    const renderElement = (page) => {
+        if (page === "start-ellipsis" || page === "end-ellipsis") {
+        return <button className='pagination1' key={page}>...</button>;
+        }
+
+        return (
+        <button className='pagination1'
+            style={{
+            backgroundColor: page === pagino.page ? "#0971f1" : ""
+            ,color: page === pagino.page? "white":""
+            }}
+            key={page}
+            onClick={() => hanglePaginoNavigation(page)}
+        >
+            {page}
+        </button>
+        );
+    };
+
+    useEffect(() => {
+        dispatch(ACTION_GET_MOVIES(pagino.page))
+    }, [pagino.page]) 
+
+
+    const navigate = useNavigate()
+    const detail = (id) => {
+        navigate(`/detailmovie/${id}`)
+    }
+
     return(
         <div>
             <Navbar/>
-            <h1>ListMoovie</h1>            
+
+            <div className='mt-3'>
+                <ul>{pages.map(renderElement)}</ul>
+            </div>
+
+            {ListMovies.loadAll===true?(
+               <div>loading</div>
+            ):(
+                <Row className='list-card'>
+                    {ListMovies.all.map((e)=>{
+                        return(
+                            <Col lg="3" md="3" xs="6" className="cardd"> 
+                                <img onClick={() => detail(e.id)} className="poster" src={`https://image.tmdb.org/t/p/w500/${e.poster_path}`}/>             
+                            </Col>
+                        )
+                    })}
+                </Row>
+            )}               
         </div>
     )
 }
 
-export default ListMoovie;
+export default ListMovie;
