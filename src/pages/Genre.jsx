45225b11e9e76231221bc39c2c28ebd1926570b1
alfiyansyah <65682948+alfiyansyahhh@users.noreply.github.com>
2022-01-11 15:59:08 +0700

    get all genre with react-redux first

--- src/pages/Genre.jsx
+++ src/pages/Genre.jsx
@@ -1,10 +1,38 @@
 import Navbar from '../components/Navbar'
+import {useEffect} from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import genre from '../redux/actions/ListGenre'
 
 const Genre = () => {
+
+    const dispatch = useDispatch()
+    const ListGenre = useSelector((state) => state.Genre)
+
+    useEffect(() => {
+        dispatch(genre.ACTION_GET_GENRES())
+    }, []) 
+
     return(
         <div>
             <Navbar/>
             <h1>Genre</h1>
+            {ListGenre.loadAll===true?(
+                <div>
+                    loading
+                </div>
+            ):(
+                <div>
+                    {ListGenre.all.map((e) => {
+                        return(
+                            <div>
+                                {e.id}-{e.name}
+                            </div>
+                        )
+                    })}
+                </div>
+            )}
+
+            
         </div>
     )
 }
