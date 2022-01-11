6ba7d2211195ba886d19c43bc04847acbc5ae012
alfiyansyah <65682948+alfiyansyahhh@users.noreply.github.com>
2022-01-11 22:42:35 +0700

    update router

--- src/router/Index.js
+++ src/router/Index.js
@@ -1,16 +1,14 @@
 import {Routes, Route} from "react-router-dom"
-import Home from '../pages/Home';
-import Genre from "../pages/Genre";
-import ListMovie from "../pages/ListMovie";
 import DetailMovie from "../pages/DetailMovie.jsx"
+import ListMovie from '../pages/ListMovie'
+import Genre from '../pages/Genre'
 
 const Router = () =>{
     return (
     <Routes>
-        <Route path="/" element={<Home />} exact/>
-        <Route path="/listmovie" element={<ListMovie />} />
-        <Route path="/genre" element={<Genre />} />
+        <Route path="/" element={<ListMovie />} exact/>
         <Route path="/detailmovie/:id" element={<DetailMovie />} />
+        <Route path="/Genre" element={<Genre />} />
     </Routes>
     );
 }
